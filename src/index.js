46599e009334b68c4763f4c00c478055fa568efb
Syth <gshnoozer2@gmail.com>
2022-01-23 22:06:04 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// these are the variables you can use as inputs to your algorithms
+console.log(fxhash)   // the 64 chars hex number fed to your algorithm
+console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
+
+// note about the fxrand() function 
+// when the "fxhash" is always the same, it will generate the same sequence of
+// pseudo random numbers, always
+
+//----------------------
+// defining features
+//----------------------
+// You can define some token features by populating the $fxhashFeatures property
+// of the window object.
+// More about it in the guide, section features:
+// [https://fxhash.xyz/articles/guide-mint-generative-token#features]
+//
+// window.$fxhashFeatures = {
+//   "Background": "Black",
+//   "Number of lines": 10,
+//   "Inverted": true
+// }
+
+//Keep this at top of file for random seed. Feel free to change the number!
+let seed = rand(1000000);
+seedRandom(seed);
+
+const frame = new Frame("FIT", 1024, 1024, "#222", "#222");
+frame.on("ready", () => {
+	const stage = frame.stage;
+	let stageW = frame.width;
+	let swageH = frame.height;
+	
+	//your code goes here
+	
+	
+	stage.update();
+});
